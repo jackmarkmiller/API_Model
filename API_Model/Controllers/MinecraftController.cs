fb0e90888baf581b285a7c3ff72591d21e695ff4
Jack Markmiller <jackmarkmiller@ou.edu>
2019-04-15 11:43:39 -0500

    participation

---
+++ API_Model/Controllers/MinecraftController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace API_Model.Controllers
+{
+    public class MinecraftController : Controller
+    {
+        // GET: Minecraft
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+       
+    }
+}
---
+++ API_Model/Models/Minecraft.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace API_Model.Models
+{
+    public class Minecraft
+    {
+        public string MobName { get; set;}
+        public string Color { get; set; }
+        public string Image{ get; set; }
+        public string Drops { get; set; }
+    }
+}
